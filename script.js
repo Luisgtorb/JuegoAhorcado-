c813f5800d3b90d2f4ef14c2c608b3a9345110ba
Luis Toro <ltoro_99@hotmail.com>
2022-09-05 18:43:10 -0400

    Agragar una nueva palabra listo

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+var palabras = ["PLAYA", "FIESTA", "PISCINA", "VERANO", "CARRO", "YATE", "MUSICA", "AGUA", "VIAJE", "PELOTA"]; 
+
+var agregarPalabra = document.querySelector(".textarea");
+
 function limpiarBotones() {
     document.getElementById("iniciar").hidden=true;
     document.getElementById("anexar").hidden=true;
+    document.getElementById("inicio-juego").hidden=true;
+}
+
+function agregarNuevaPalabra() {
+    document.getElementById("input-palabra").hidden=false;
 }
 
 function btnIniciar() {
@@ -9,4 +18,16 @@ function btnIniciar() {
 
 function btnAnexar() {
     limpiarBotones();
+    agregarNuevaPalabra();
+}
+
+function btnNuevaPalabra() {
+   palabras.push(agregarPalabra.value);
+   agregarPalabra.value = " ";
+   agregarPalabra.focus();
+   console.log(palabras);
+}
+
+function btnCancelar() {
+
 }
