cd75a095e2ffd6a7f80b01ff1e8c3a0253ce9ea9
Luis Toro <ltoro_99@hotmail.com>
2022-09-05 16:14:11 -0400

    Primer check point listo

---
+++ script.js
@@ -0,0 +1,12 @@
+function limpiarBotones() {
+    document.getElementById("iniciar").hidden=true;
+    document.getElementById("anexar").hidden=true;
+}
+
+function btnIniciar() {
+    limpiarBotones();
+}
+
+function btnAnexar() {
+    limpiarBotones();
+}
