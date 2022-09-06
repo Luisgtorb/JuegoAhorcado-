963f824cd039d519cf24e973eaca36e923df89bd
Luis Toro <ltoro_99@hotmail.com>
2022-09-06 11:31:21 -0400

    Modificando archivo index.html, style.css y script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-var palabras = ["PLAYA", "FIESTA", "PISCINA", "VERANO", "CARRO", "YATE", "MUSICA", "AGUA", "VIAJE", "PELOTA"]; 
+//  Seccion de Variables globales
 
+var palabras = ["PLAYA", "FIESTA", "PISCINA", "VERANO", "CARRO", "YATE", "MUSICA", "AGUA", "VIAJE", "PELOTA"]; 
 var agregarPalabra = document.querySelector(".textarea");
+var pantalla = document.querySelector(".pantalla");
+
+// Seccion de funciones adicionales
 
 function limpiarBotones() {
     document.getElementById("iniciar").hidden=true;
@@ -12,14 +16,36 @@ function agregarNuevaPalabra() {
     document.getElementById("input-palabra").hidden=false;
 }
 
+function agregarJuego() {
+    document.getElementById("juego").hidden=false;
+}
+
+function agregarInicio() {
+    document.getElementById("iniciar").hidden=false;
+    document.getElementById("anexar").hidden=false;
+    document.getElementById("inicio-juego").hidden=false;
+}
+
+// Seccion principal, botones principales
+
 function btnIniciar() {
     limpiarBotones();
+    agregarJuego();
 }
 
 function btnAnexar() {
     limpiarBotones();
     agregarNuevaPalabra();
 }
+
+// Seccion para el juego, botonoes y canvas
+
+function btnDesistir() {
+    agregarInicio();
+    document.getElementById("juego").hidden=true;
+}
+
+// Secccion para agregar nueva palabra
 
 function btnNuevaPalabra() {
    palabras.push(agregarPalabra.value);
@@ -29,8 +55,6 @@ function btnNuevaPalabra() {
 }
 
 function btnCancelar() {
-    document.getElementById("iniciar").hidden=false;
-    document.getElementById("anexar").hidden=false;
-    document.getElementById("inicio-juego").hidden=false;
+    agregarInicio();
     document.getElementById("input-palabra").hidden=true;
 }
